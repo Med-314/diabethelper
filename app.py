02f5e67ddcad69bea58567f6367d88285709cd8a
abdessamad-makan <152408174+abdessamad-makan@users.noreply.github.com>
2024-03-21 06:00:56 +0000

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request, session
 
 from chatbot import *
 
+
+
 app = Flask(__name__,template_folder='login_registration/src/main/webapp')
 app.static_folder = 'static'
 
 
 
 @app.route("/")
+
 def home():
+    
     
     #username = session.get('uname', 'Unknown Username or Guest')
     username = request.args.get('uname')
@@ -19,12 +23,14 @@ def home():
     #return render_template("login_registration/src/main/webapp/index.html")
 
 @app.route("/get")
+
 def get_bot_response():
     userText = request.args.get('msg')
     print('usermail inside get bot response: {}'.format(usermail))
     return chatbot_response(userText)
 
 @app.route('/usermail', methods=['GET'])
+
 def receive_usermail():
     
     global usermail
